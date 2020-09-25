9e718237a34ffebf020f590ac63401f76a5a22e8
donamondal <donamondal25@gmail.com>
2020-09-25 21:38:24 +0530

    modify

--- app/src/main/java/com/groupG/teleID/MainActivity.java
+++ app/src/main/java/com/groupG/teleID/MainActivity.java
@@ -1,6 +1,14 @@
 package com.groupG.teleID;
-
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.view.View;
+import android.widget.Button;
 
 import android.os.Bundle;
 
@@ -10,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+
+        System.out.print("----------------");
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:0377778888"));
+
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.CALL_PHONE},0);
+                    System.out.print("----------------");
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
     }
 }
